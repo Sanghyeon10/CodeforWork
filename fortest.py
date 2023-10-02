b0d8b44c47659555a096cfce50f80d018913b0f4
Sanghyeon10 <130223997+Sanghyeon10@users.noreply.github.com>
2023-10-02 19:06:44 +0900

    Delete fortest.py

--- fortest.py
+++
@@ -1,7 +0,0 @@
-my_list = [1, 2, 3, 4,"a", 5, 3, 6]
-
-# 특정 요소(예: 3)의 첫 번째 인덱스를 찾기
-element_to_find = "ㅍ"
-index = my_list.index(element_to_find)
-
-print(f"{element_to_find}는 리스트의 {index}번째에 위치합니다.")
